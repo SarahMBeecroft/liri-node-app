6fbffc6d8af0293fffdaee24455c56c72990b9ee
Sarah B <smblair87@gmail.com>
2019-04-27 08:04:17 -0700

    Updated variables on concert-this function

--- liri.js
+++ liri.js
@@ -13,44 +13,48 @@ var moment = require ("moment");
 // Process arguments
 var command = process.argv[2];
 console.log(process.argv[2]);
-// var userResponse = process.argv[3];
+var userResponse = process.argv[3];
+console.log(process.argv[3]);
+
 
 // If statements for commands
 if (command === 'concert-this') {
-  concertThis(artist);
+  concertThis(userResponse);
+} else {
+  concertThis("Radiohead");
 }
 
-var artist = process.argv[3];
+
 
 // All functions
 
 // Concert this
-function concertThis(artist) {
-
-
+function concertThis(userResponse) {
 
 // Axios call
-  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+  axios.get("https://rest.bandsintown.com/artists/" + userResponse + "/events?app_id=codingbootcamp")
   .then( function(response) {
       console.log(response);
 
       for (var i = 0; i < response.data.length; i++) {
 
+        console.log("Venue: " + response[i].venue.name);
+
         // Saves date and time response in a variable
-        var dateTime = response.data[i].dateTime;
+        var dateTime = response[i].dateTime;
 
         // Splits the date and time
-        var dateTimeArr = dataTime.split('T'); 
+        var dateTimeArr = dateTime.split('T'); 
 
         // Variable for concert results 
         var concertResults = 
 
-        "\nVenue Name: " + response.data[i].venue.name +
-        "\nVenue Location: " + response.data[i].venue.city +
+        "\nVenue Name: " + response[i].venue.name +
+        "\nVenue Location: " + response[i].venue.city +
         "\nDate of Event: " + moment(dateTimeArr[0], "YYYY-DD-MM").format("DD/MM/YYYY");
 
         console.log(concertResults);
-      }
+      };
 
   })
 
