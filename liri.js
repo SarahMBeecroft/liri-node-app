551c49fdf5a38f4a42b5116ca8e5a98b5fb25621
Sarah B <smblair87@gmail.com>
2019-04-27 20:58:12 -0700

    Added keys for Spotify and function to search tracks, still working on how to get a preview link from API

--- liri.js
+++ liri.js
@@ -16,22 +16,29 @@ var moment = require("moment");
 //===============================================//
 var command = process.argv[2];
 console.log(process.argv[2]);
-var userResponse = process.argv[3];
-console.log(process.argv[3]);
+var userResponse = process.argv.slice(3).join(" "); // slice allows for space
+console.log(process.argv.slice(3).join(" "));
 
 // If statements for commands
 //===============================================//
+// concert-this
 if (command === 'concert-this') {
   concertThis(userResponse);
 } else {
   concertThis("Beck");
 }
 
-
+// spotify-this-song
+if (command === 'spotify-this-song') {
+  spotifyThis(userResponse);
+} else {
+  spotifyThis("The Sign");
+}
 
 // All functions
 //===============================================//
 // Concert this
+//===============================================//
 function concertThis(userResponse) {
 
   // Axios call
@@ -46,6 +53,27 @@ function concertThis(userResponse) {
 
     console.log("\nVenue Name: " + concertResults.venue.name + "\nVenue Location: " + concertResults.venue.city + "\nDate of Event: " + time);
 
+  });
+
+};
+
+// Spotify this song
+//===============================================//
+function spotifyThis(userResponse) {
+ 
+  spotify.search({ type: 'track', query: userResponse}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }else {
+      for (var i = 0; i < data.tracks.items.length; i++){
+
+      // Setting artist index to 0 returns all results, still need to find a way to get preview link
+      console.log("Artist: " + data.tracks.items[i].artists[0].name + "\nSong Name: "
+      + data.tracks.items[i].name + "\nPreview Link: " + data.tracks.items[i].link + "\nAlbum Name: " + data.tracks.items[i].album.name + "\n");
+      
+      };
+    }
+   
   });
 
 };
